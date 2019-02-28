05e93b521ec382b34549645952c3a159b72a2133
Mario Torres <mariotee10@gmail.com>
2019-02-28 16:32:50 -0600

    Updates

---
+++ precache-manifest.0b770e1d8f690e560475664bad43d3f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "391f50b85fc8eee42162",
+    "url": "/DadJokes/static/js/runtime~main.391f50b8.js"
+  },
+  {
+    "revision": "7d15e4003d9dd7c2dc95",
+    "url": "/DadJokes/static/js/main.7d15e400.chunk.js"
+  },
+  {
+    "revision": "32c16021b3bfcf0fc19f",
+    "url": "/DadJokes/static/js/1.32c16021.chunk.js"
+  },
+  {
+    "revision": "7d15e4003d9dd7c2dc95",
+    "url": "/DadJokes/static/css/main.86fa037d.chunk.css"
+  },
+  {
+    "revision": "cab31272b076343f7a325c959b3d7186",
+    "url": "/DadJokes/index.html"
+  }
+];
